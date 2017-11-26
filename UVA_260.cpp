4a17ef47661c7373ef1463ca50bdb9e11b535b37
sourabh2311 <sourabhaggarwal231196@gmail.com>
2017-11-26 14:36:33 +0530

    Create UVA_260.cpp

---
+++ UVA_260.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string line[205];
+string mod1[205];
+int N;
+int dr[] = {-1, -1, 0, 0, 1, 1};
+int dc[] = {-1, 0, -1, 1, 0, 1};
+
+void floodfill(int r, int c, char c1, char c2)
+{
+    if(r < 0 || r >= N || c < 0 || c >= N || mod1[r][c] != c1) return;
+    mod1[r][c] = c2;
+    for(int i = 0; i < 6; i++) {
+        floodfill(r + dr[i], c + dc[i], c1, c2);
+    }
+}
+
+int main()
+{
+    int t = 1;
+    while(cin >> N && N != 0)
+    {
+        for(int i = 0; i < N; i++)
+        {
+            cin >> line[i];
+            mod1[i] = line[i];
+            mod2[i] = line[i];
+        }
+        cout << t++ << " ";
+        bool black = false, white = false;
+        /* For black */
+        for(int i = 0; i < N; i++)
+        {
+            bool check1 = false, check2 = false;
+            floodfill(0, i, 'b', '.');
+            for(int j = 0; j < N; j++)
+            {
+                if(mod1[0][j] == '.') check1 = true;
+                if(mod1[N - 1][j] == '.') check2 = true;
+            }
+            floodfill(0, i, '.', 'b');
+            if(check1 && check2)
+            {
+                black = true;
+                break;
+            }
+        }
+        if(black)
+        {
+            cout << "B\n";
+        }
+        else
+        {
+            cout << "W\n";
+        }
+    }
+    return 0;
+}
