0ea12ab43230c74fbf8ec6a60f34277b7f841b1a
sourabh2311 <sourabhaggarwal231196@gmail.com>
2017-12-10 13:23:49 +0530

    Create UVA_11646.cpp

---
+++ UVA_11646.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define EPS 1e-5
+double PI = 2 * acos(0);
+
+
+double a, b;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //freopen("ina.txt", "r", stdin);
+    //freopen("outa.txt", "w", stdout);
+    cout << fixed << setprecision(10);
+    char c;
+    int t = 1;
+    while(cin >> a >> c >> b) {
+        double theta = 2 * asin(b / sqrt(a * a + b * b));
+        double k = 400 / (2 * a + (sqrt(a * a + b * b) * theta));
+        double length = a * k, width = b * k;
+        cout << "Case " << t++ << ": " << length << " " << width << "\n";
+
+    }
+    return 0;
+}
