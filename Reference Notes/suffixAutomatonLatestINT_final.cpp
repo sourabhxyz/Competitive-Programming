46a56e51f252ee12c29813c23bf6a5068350a6bc
sourabh2311 <sourabhaggarwal231196@gmail.com>
2018-12-10 18:23:08 +0530

    10/12/18-2

--- Reference Notes/suffixAutomatonLatestINT_final.cpp
+++ Reference Notes/suffixAutomatonLatestINT_final.cpp
@@ -69,7 +69,7 @@ void sa_extend (char c) {
     }
     last = cur;
 }
-
+// A state v will correspond to set of endpos equivalent strings, cnt[v] will give the number of occurences of such strings
 void processcnt() {
     int maxlen = st[last].len;
     for(int i = maxlen; i >= 0; i--) {
@@ -78,6 +78,8 @@ void processcnt() {
         }
     }
 }
+
+// Clearly suffixes should be marked as terminal
 void processterminal() {
     isterminal[last] = true;
     int p = st[last].link;
@@ -87,6 +89,7 @@ void processterminal() {
     }
 }
 
+// Gives the number of substrings (not necessarily distinct). Clearly it should return n.(n+1)/2
 int processnumsubs(int at) {
     if(nsubs[at] != 0) return nsubs[at];
     nsubs[at] = st[at].cnt;
@@ -107,14 +110,20 @@ void constructSA(string ss) {
         st[st[v].link].inv_link.push_back(v);
     processnumsubs(0);
 }
-
-bool exist(string &tosearch) {
+// ---------------------------------------After SA Construction
+//
+int getcorrstate(string &tosearch) {
     int at = 0;
     for (int i = 0; i < tosearch.size(); i++) {
-        if (!st[at].count (tosearch[i])) return false;
+        if (!st[at].count (tosearch[i])) return -1;
         at = st[at].next[tosearch[i]];
     }
-    return true;
+    return at;
+}
+
+bool exist(string &tosearch) {
+    int at = getcorrstate (tosearch);
+    return at == -1 ? false : true;
 }
 
 // Returns number of different substrings = number of paths in DAG. And number of paths is clearly not a function of number of states in DAG.
@@ -166,54 +175,27 @@ void kthlexo2(int at, int k, string &as) {
 }
 // Returns true is the given string is the suffix of T
 bool issuffix(string &tosearch) {
-    int at = 0;
-    for (int i = 0; i < tosearch.size(); i++) {
-        if (!st[at].count (tosearch[i])) return false;
-        at = st[at].next[tosearch[i]];
-    }
-    if(isterminal[at]) return true;
-    else return false;
+    int at = getcorrstate (tosearch);
+    return isterminal[at];
 }
 
-//Returns how many times P enters in T (occurences can overlap)
-/*for each state v of the machine calculate a number 'cnt[v]' which is equal to the
- * size of the set endpos(v). In fact, aint the rows corresponding to the same state
+// Returns how many times P enters in T (occurences can overlap)
+/* for each state v of the machine calculate a number 'cnt[v]' which is equal to the
+ * size of the set endpos(v). In fact, all the strings corresponding to the same state
  * enter the T same number of times which is equal to the number of positions in the set
  * endpos. */
-int numoccur(string tosearch) {
-    int at = 0;
-    for (int i = 0; i < tosearch.size(); i++) {
-        if(st[at].next.find(tosearch[i]) == st[at].next.end()) {
-            return 0;
-        }
-        at = st[at].next[tosearch[i]];
-    }
-    return st[at].cnt;
+int numoccur(string &tosearch) {
+    int at = getcorrstate (tosearch);
+    return at == -1 ? 0 : st[at].cnt;
 }
 
-//Return position of the first occurrence of substring in T
-int firstpos(string tosearch) {
-    int at = 0;
-    for (int i = 0; i < tosearch.size(); i++) {
-        if(st[at].next.find(tosearch[i]) == st[at].next.end()) {
-            return -1;
-        }
-        at = st[at].next[tosearch[i]];
-    }
+// Return position of the first occurrence of substring in T
+int firstpos(string &tosearch) {
+    int at = getcorrstate (tosearch);
     return st[at].firstpos - tosearch.size() + 1;
+}
 
-}
-//Returns Positions of aint occurrences of substring in T
-int getcorrstate(string tosearch) {
-    int at = 0;
-    for (int i = 0; i < tosearch.size(); i++) {
-        if(st[at].next.find(tosearch[i]) == st[at].next.end()) {
-            return -1;
-        }
-        at = st[at].next[tosearch[i]];
-    }
-    return at;
-}
+// Returns Positions of all occurrences of substring in T
 void output_all_occurences (int v, int P_length) {
     if (!st[v].is_clon)
         cout << st[v].firstpos - P_length + 1 << "\n";
@@ -221,11 +203,11 @@ void output_all_occurences (int v, int P_length) {
         output_all_occurences(st[v].inv_link[i], P_length);
 }
 
-void smallestcyclicshift(const string ss) {
+void smallestcyclicshift(int n) {
     int at = 0;
     string anss;
     int length = 0;
-    while(length != ss.size()) {
+    while(length != n) {
         for (auto it : st[at].next) {
             anss.push_back(it.first);
             at = it.second;
@@ -233,7 +215,8 @@ void smallestcyclicshift(const string ss) {
             break;
         }
     }
-    cout << st[at].firstpos - ss.size() + 2 << "\n";
+    cout << anss << "\n";
+    // cout << st[at].firstpos - n + 1 << "\n"; may give the index for that shift.
 }
 
 
@@ -242,99 +225,96 @@ int main() {
     cin >> s;
     constructSA(s);
     int choice;
-    while(true){
-        cout << "Choose your option:\n1: Substring exist in T or not\n2: Number of different substring of T\n";
-        cout << "3: To find total length of distinct substrings\n";
-        cout << "4: To check whether the given string is suffix or not\n";
-        cout << "5(5.1): To print the K-th lexicographic substring (Repeated substrings allowed)\n";
-        cout << "6: To see how many times, given string occurs in T\n";
-        cout << "7: To find the position of the first occurrence of substring in T\n";
-        cout << "8: To find position of all the occurences of substring in T\n";
-        cout << "9(5.2): To print the K-th lexicographic substring (Repeated substrings not allowed)\n";
-        cout << "10: To find the smallest cyclic shift\n";
+    cout << "Choose your option:\n1: Substring exist in T or not\n2: Number of different substring of T\n";
+    cout << "3: To find total length of distinct substrings\n";
+    cout << "4: To check whether the given string is suffix or not\n";
+    cout << "5(5.1): To print the K-th lexicographic substring (Repeated substrings allowed)\n";
+    cout << "6: To see how many times, given string occurs in T\n";
+    cout << "7: To find the position of the first occurrence of substring in T\n";
+    cout << "8: To find position of all the occurences of substring in T\n";
+    cout << "9(5.2): To print the K-th lexicographic substring (Repeated substrings not allowed)\n";
+    cout << "10: To find the smallest cyclic shift\n";
 
-        cout << "15: to exit\n";
-        cin >> choice;
-        if(choice == 15) break;
-        string ss, ns;
-        int k, v;
-        switch(choice) {
-            case 1:
-                cout << "Enter your string\n";
-                cin >> ss;
-                if (exist(ss)) {
-                    cout << "yes it exist\n";
-                } else {
-                    cout << "no it does not exist\n";
-                }
-                //cout << "Enter new to string to search for\n";
-                break;
-            case 2:
-                cout << numdiffsub(0) - 1 << "\n";
-                break;
-            case 3:
-                numdiffsub(0);
-                cout << totlength(0) << "\n";
-                break;
-            case 4:
-                cout << "Enter the string\n";
-                cin >> ss;
-                if(issuffix(ss)) cout << "yes\n";
-                else cout << "no\n";
-                break;
-            case 5:
-                cin >> k;
-                ss.clear();
-                kthlexo(0, k, ss);
-                if(ss.empty()) {
-                    ss = "No such line.";
-                }
-                cout << ss << "\n";
-                break;
-            case 6:
-                cout << "Enter string\n";
-                cin >> ss;
-                cout << numoccur(ss) << "\n";
-                break;
-            case 7:
-                cout << "Enter string\n";
-                cin >> ss;
-                cout << firstpos(ss) << "\n";
-                break;
-            case 8:
-                cout << "Enter string\n";
-                cin >> ss;
-                /*for(v = 0; v < s.size(); v++) {
-                    cout << setw(2) << v;
-                }
-                cout << "\n";
-                for(v = 0; v < s.size(); v++) {
-                    cout << setw(2) << s[v];
-                }
-                cout << "\n";*/
-                v = getcorrstate(ss);
-                if(v != -1) {
-                    output_all_occurences(v, ss.size());
-                }
-                break;
-            case 9:
-                cin >> k;
-                ss.clear();
-                numdiffsub(0);
-                kthlexo2(0, k, ss);
-                if(ss.empty()) {
-                    ss = "No such line.";
-                }
-                cout << ss << "\n";
-                break;
-            case 10:
-                cout << "Enter S\n";
-                cin >> ss;
-                s = ss + ss;
-                constructSA(s);
-                smallestcyclicshift(ss);
-                break;
-        }
+    cout << "15: to exit\n";
+    cin >> choice;
+    if(choice == 15) break;
+    string ss, ns;
+    int k, v;
+    switch(choice) {
+        case 1:
+            cout << "Enter your string\n";
+            cin >> ss;
+            if (exist(ss)) {
+                cout << "yes it exist\n";
+            } else {
+                cout << "no it does not exist\n";
+            }
+            //cout << "Enter new to string to search for\n";
+            break;
+        case 2:
+            cout << numdiffsub(0) - 1 << "\n";
+            break;
+        case 3:
+            numdiffsub(0);
+            cout << totlength(0) << "\n";
+            break;
+        case 4:
+            cout << "Enter the string\n";
+            cin >> ss;
+            if(issuffix(ss)) cout << "yes\n";
+            else cout << "no\n";
+            break;
+        case 5:
+            cin >> k;
+            ss.clear();
+            kthlexo(0, k, ss);
+            if(ss.empty()) {
+                ss = "No such line.";
+            }
+            cout << ss << "\n";
+            break;
+        case 6:
+            cout << "Enter string\n";
+            cin >> ss;
+            cout << numoccur(ss) << "\n";
+            break;
+        case 7:
+            cout << "Enter string\n";
+            cin >> ss;
+            cout << firstpos(ss) << "\n";
+            break;
+        case 8:
+            cout << "Enter string\n";
+            cin >> ss;
+            /*for(v = 0; v < s.size(); v++) {
+                cout << setw(2) << v;
+            }
+            cout << "\n";
+            for(v = 0; v < s.size(); v++) {
+                cout << setw(2) << s[v];
+            }
+            cout << "\n";*/
+            v = getcorrstate(ss);
+            if(v != -1) {
+                output_all_occurences(v, ss.size());
+            }
+            break;
+        case 9:
+            cin >> k;
+            numdiffsub(0);
+            kthlexo2(0, k, ss);
+            if(ss.empty()) {
+                ss = "No such line.";
+            }
+            cout << ss << "\n";
+            break;
+        case 10:
+            cout << "Enter S\n";
+            cin >> ss;
+            s = ss + ss;
+            constructSA(s);
+            smallestcyclicshift(ss.size ());
+            break;
     }
     return 0;
 }
